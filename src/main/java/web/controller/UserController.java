6707a572e56f4d14f9b2a3fad3f12320195dc9b2
Anton Balakhonkin <anton-kazan@bk.ru>
2021-11-13 22:14:24 +0300

    добавлен security

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -3,12 +3,16 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.service.UserService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@RequestMapping("/users")
+@RequestMapping("")
 public class UserController {
     private final UserService userService;
 
@@ -17,45 +21,60 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping()
-    public String getUsers(Model model){
-        model.addAttribute("users", userService.getUsers());
-        return "users/users";
+//    @GetMapping()
+//    public String getUsers(Model model){
+//        model.addAttribute("users", userService.getUsers());
+//        return "users/users";
+//    }
+//
+//    @GetMapping("/new")
+//    public String newUser(@ModelAttribute("user") User user) {
+//        //model.addAttribute("user", new User());
+//        return "users/new";
+//    }
+//
+//    @PostMapping()
+//    public String create(@ModelAttribute("user") User user) {
+//        userService.save(user);
+//        return "redirect:/users";
+//    }
+//
+//    @GetMapping("/{id}/edit")
+//    public String edit(Model model, @PathVariable("id") int id) {
+//        model.addAttribute("user", userService.show(id));
+//        return "users/edit";
+//    }
+//
+//    @PatchMapping("/{id}")
+//    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+//        userService.update(id, user);
+//        return "redirect:/users";
+//    }
+//
+//    @GetMapping("/{id}")
+//    public String show(@PathVariable("id") int id, Model model) {
+//        model.addAttribute("user", userService.show(id));
+//        return "users/show";
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public String delete(@PathVariable("id") int id) {
+//        userService.delete(id);
+//        return "redirect:/users";
+//    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Hello!");
+        messages.add("I'm Spring MVC-SECURITY application");
+        messages.add("5.2.0 version by sep'19 ");
+        model.addAttribute("messages", messages);
+        return "hello";
     }
 
-    @GetMapping("/new")
-    public String newUser(@ModelAttribute("user") User user) {
-        //model.addAttribute("user", new User());
-        return "users/new";
-    }
-
-    @PostMapping()
-    public String create(@ModelAttribute("user") User user) {
-        userService.save(user);
-        return "redirect:/users";
-    }
-
-    @GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") int id) {
-        model.addAttribute("user", userService.show(id));
-        return "users/edit";
-    }
-
-    @PatchMapping("/{id}")
-    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
-        userService.update(id, user);
-        return "redirect:/users";
-    }
-
-    @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model) {
-        model.addAttribute("user", userService.show(id));
-        return "users/show";
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id) {
-        userService.delete(id);
-        return "redirect:/users";
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String loginPage() {
+        return "login";
     }
 }
